e66267bf65fa0e5c2ada2fe860a3b0ba50eb8e3d
Shuayb Ali <shuaybali2023@gmail.com>
2026-07-02 16:34:04 +0000

    feat(docker): build Flask and MySQL multi-container application

--- 05-docker/projects/flask-mysql/app.py
+++ 05-docker/projects/flask-mysql/app.py
@@ -1,24 +1,34 @@
-# app.py
-
 from flask import Flask
 import MySQLdb
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    # Connect to the MySQL database
-    db = MySQLdb.connect(
-        host="mydb",    # Hostname of the MySQL container
-        user="root",    # Username to connect to MySQL
-        passwd="my-secret-pw",  # Password for the MySQL user
-        db="mysql"      # Name of the database to connect to
-    )
+DB_HOST = os.getenv("DB_HOST", "mydb")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "my-secret-pw")
+DB_NAME = os.getenv("DB_NAME", "mysql")
+PORT = int(os.getenv("PORT", 5002))
+
+
+@app.route("/")
+def home():
+    db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWORD, db=DB_NAME)
+
     cur = db.cursor()
     cur.execute("SELECT VERSION()")
     version = cur.fetchone()
-    return f'Hello, World! MySQL version: {version[0]}'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    cur.close()
+    db.close()
+
+    return f"""
+    <h1>Flask + MySQL</h1>
+    <p>This Flask app is running in one container and connecting to MySQL in another container.</p>
+    <p><strong>MySQL Version:</strong> {version[0]}</p>
+    """
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT)
 
